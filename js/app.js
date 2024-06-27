3cf41b69a9753e516357afbeb8a08fe87b2a3ad8
Yeicam24 <yeicam24@gmail.com>
2024-06-27 16:54:14 -0500

    correcciones

--- js/app.js
+++ js/app.js
@@ -1,42 +1,62 @@
 
-
-
+let imagenRespuesta = document.getElementById('img-respuesta');
+let tituloRespuesta = document.getElementById('titulo-respuesta');
+let parrafoRespuesta = document.getElementById('parrafo-respuesta')
+// FUNCION PARA ENCRIPTAR 
 function encriptar(){
-        let entradaTexto = document.getElementById('entradaTexto').value;
-       
-        let salidaEncriptada =  entradaTexto.replace(/e/g,'enter').replace(/i/g,'imes').replace(/a/g, 'ai').replace(/o/g,'ober').replace(/u/g,'ufat');
-        
-        if (salidaEncriptada.length !=0) {
-            document.getElementById('entradaTexto').value = salidaEncriptada;
-            document.getElementById('img-respuesta').src = './img/img-block.png';
-            cambiarElementos('#titulo-respuesta','Elemento encriptado con exito');
-            cambiarElementos('#parrafo-respuesta', '');
-
-        }else{
-            alert('No se encontro ningun mensaje');
-        }
-        
-        //return console.log(salidaEncriptada);
+   
+    let entradaTexto = document.getElementById('entradaTexto').value;
     
-}
-function desencriptar(){
-    let entradaTextoEncriptado = document.getElementById('entradaTexto').value;
+    let salidaEncriptada =  entradaTexto.replace(/e/img,'enter').replace(/i/img,'imes').replace(/a/img, 'ai').replace(/o/img,'ober').replace(/u/img,'ufat');
+    
+    if (entradaTexto.length !=0) {
+        document.getElementById('entradaTexto').value = '';
+        
+        
+        cambiarElementos('#respuesta', salidaEncriptada);
+        // ELEMENTOS QUE APRECEN Y DESAPARECEN 
+        imagenRespuesta.remove();
+        tituloRespuesta.remove();
+        parrafoRespuesta.remove();
+        
+        document.getElementById('btn-copiar').classList.remove('copiar');
 
-    let salidaDesencriptada = entradaTextoEncriptado.replace(/enter/g,'e').replace(/imes/g,'i').replace(/ai/g,'a').replace(/ober/g,'o').replace(/ufat/g,'u');
-    if (entradaTextoEncriptado.length !=0) {
-        document.getElementById('entradaTexto').value = salidaDesencriptada;
     }else{
         alert('No se encontro ningun mensaje');
     }
-
-
-
 }
 
+// FUNCION PARA DESENCRIPTAR 
+function desencriptar(){
+    let entradaTextoEncriptado = document.getElementById('entradaTexto').value.toLowerCase();
 
+    let salidaDesencriptada = entradaTextoEncriptado.replace(/enter/img,'e').replace(/imes/img,'i').replace(/ai/img,'a').replace(/ober/img,'o').replace(/ufat/img,'u');
+    if (entradaTextoEncriptado.length !=0) {
+        
+        
+        cambiarElementos('#respuesta', salidaDesencriptada);
+        
+        imagenRespuesta.remove();
+        tituloRespuesta.remove();
+        parrafoRespuesta.remove();
 
+        document.getElementById('btn-copiar').classList.remove('copiar');
+        document.getElementById('entradaTexto').value = '';
+        
+    }else{
+        alert('No se encontro ningun mensaje');
+    }
+    
+}
 
+// FUNCION PARA COPEAR EL TEXTO 
+function copiar(){
+    let texto = document.getElementById('respuesta');
+    let btnCopiar = document.getElementById('btn-copiar');
 
+    navigator.clipboard.writeText(texto.textContent);
+    btnCopiar.textContent = 'Copeado';
+}
 
 
 function cambiarElementos(elemento, texto){
